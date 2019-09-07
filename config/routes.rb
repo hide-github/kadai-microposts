0c3b8a6278e635a51e55aa3e34d9979df0e19078
hide <snowbear.github@gmail.com>
2019-09-07 08:40:58 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  #トップページ用
   root to: 'toppages#index'
   
+  #ログイン用
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  #その他
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :new, :create]
 end
